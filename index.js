329c882d4114e8b075b4848d5f6025287bab5260
Matt <aboutmattlaw@gmail.com>
2021-09-05 17:24:36 -0400

    commit tree 5fdcdc25c981e2a055ef6cea5eef5074737bb5ec

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// add(2,3); //=> 5
+
+
+const divide = function divide(){
+    return 2000 / 100
+}
+
+const square = (x) => x * x
+
+const add = (a,b) => a + b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// first attempt 
+
+
+
+// const divide = function divide(){
+//     return 2000 / 100
+// }
+
+// const square = (x) => x * x;
+
+// const add = (a , b) => a + b;
